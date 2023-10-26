bbf9c8c40bf288e0c873e669c51e0743b3593602
Yuriy Lebedinskiy <yurleb@linuxprac.box.office.basealt.ru>
2023-10-26 10:11:41 +0000

    Deleted generates

--- 06_TracingFencing/test5.sh
+++ 06_TracingFencing/test5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "This is a test input file" > test_input.txt
-strace -e unlink -e fault=unlink:error=ENOENT ./move test_input.txt test_output.txt  > /dev/null 2> /dev/null
+strace -e unlink -e fault=unlink:error=ENOENT ./move test_input.txt test_output.txt > /dev/null 2> /dev/null
 
 if [ $? == 4 ]; then
 	echo "Test checked succesfully"
--- 06_TracingFencing/test6.sh
+++ 06_TracingFencing/test6.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "This is a test input file" > PROTECTED_test_input.txt
-LD_PRELOAD=./libprotect.so ./move PROTECTED_test_input.txt test_output.txt
+LD_PRELOAD=./libprotect.so ./move PROTECTED_test_input.txt test_output.txt > /dev/null 2> /dev/null
 
 if cmp PROTECTED_test_input.txt test_output.txt
     then echo "Test checked succesfully"
